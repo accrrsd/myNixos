6d72e59d2d4ffee7cea2ec2f67008b5619cb6b46
Daniel <accrrsd@bk.ru>
2026-05-15 15:41:01 +0300

    add color sheme argument

--- user-modules/matugen/scripts/select-wallpaper.sh
+++ user-modules/matugen/scripts/select-wallpaper.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCHEME_NAME="${1:-content}"
+
 wall_dirs=(
     "/nixos-config/src/predefined-wallpaper"
     "$HOME/Pictures/Wallpapers"
@@ -35,17 +37,18 @@ done | shuf | rofi -wayland -dmenu -theme "$rofi_theme" -theme-str "element-icon
 
 [[ -n "$wall_selection" ]] || exit 1
 
-# scheme-tonal-spot  - Default Material You palette. Balanced and soft.
-# scheme-fidelity    - Matches the source color as closely as possible.
-# scheme-content     - Optimized for content-based colors, natural look.
-# scheme-expressive  - High contrast, vibrant, and bold accent colors.
-# scheme-fruit-salad - High saturation and diverse "playful" color mix.
-# scheme-rainbow     - Vivid colors with minimal neutral/gray tones.
-# scheme-neutral     - Desaturated, grayscale-heavy, minimalist palette.
-# scheme-monochrome  - Shades of a single color for a clean, uniform look.
+# Possible schemes (without scheme- prefix)
+# tonal-spot  - Default Material You palette. Balanced and soft.
+# fidelity    - Matches the source color as closely as possible.
+# content     - Optimized for content-based colors, natural look.
+# expressive  - High contrast, vibrant, and bold accent colors.
+# fruit-salad - High saturation and diverse "playful" color mix.
+# rainbow     - Vivid colors with minimal neutral/gray tones.
+# neutral     - Desaturated, grayscale-heavy, minimalist palette.
+# monochrome  - Shades of a single color for a clean, uniform look.
 
 swww img "$wall_selection" --transition-step 10 --transition-fps 30 --transition-type any &
-matugen image "$wall_selection" --source-color-index 0 -t scheme-content & sleep 0.2
+matugen image "$wall_selection" --source-color-index 0 -t "scheme-$SCHEME_NAME" & sleep 0.2
 
 if pgrep -x "waybar" > /dev/null; then
     pkill waybar
