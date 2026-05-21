b23a044272c631d4ef15798522411d8267cf8e9f
Daniel <accrrsd@bk.ru>
2026-05-21 22:26:41 +0300

    flake update + script update

--- scripts/QoL/flake-update.sh
+++ scripts/QoL/flake-update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# usage example: ./flake-update.sh [pc-name] (optional)
+# usage example: ./flake-update.sh [pc-name] [package1 package2 ...] (optional)
+# if you need update packages only - you can use "" as first argument. ./flake-update.sh "" [package1 package2]
 
 set -euo pipefail
 
@@ -7,4 +8,16 @@ clear
 
 TARGET_HOST="${1:-$HOSTNAME}"
 
-nix flake update --flake "/nixos-config/systems/${TARGET_HOST}/flake"
+if [ $# -gt 0 ]; then
+    shift
+fi
+
+FLAKE_DIR="/nixos-config/systems/${TARGET_HOST}/flake"
+
+if [ $# -gt 0 ]; then
+    echo "Update some packages inside ${TARGET_HOST}: $*"
+    nix flake update --flake "${FLAKE_DIR}" "${@/#/--update-input }"
+else
+    echo "Update all packages inside ${TARGET_HOST}..."
+    nix flake update --flake "${FLAKE_DIR}"
+fi
