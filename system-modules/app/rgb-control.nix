e41c7c162918a32b9809bcbb0686becb26291c26
Daniel <accrrsd@bk.ru>
2026-06-23 00:30:03 +0300

    fix openrgb lian li

--- system-modules/app/rgb-control.nix
+++ system-modules/app/rgb-control.nix
@@ -1,7 +1,20 @@
 { pkgs, ... }: 
 {
-  services.udev.packages = [ pkgs.openrgb-with-all-plugins ];
+  services.udev.packages = [ 
+    pkgs.openrgb-with-all-plugins 
+    (pkgs.writeTextFile {
+      name = "99-zz-lianli-razer-override";
+      destination = "/etc/udev/rules.d/99-zz-lianli-razer-override.rules";
+      text = ''
+        # Prevent openrazer's razer_mount from hijacking the Lian Li case
+        SUBSYSTEM=="hid", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0f13", RUN="/bin/sh -c 'true'"
+      '';
+    })
+  ];
+
   boot.kernelModules = [ "i2c-dev" ];
+  # Fix openrgb - razer conflict. Force Lian Li O11 Dynamic Razer Edition (all interfaces) to hid-generic.
+  boot.kernelParams = [ "usbhid.quirks=0x1532:0x0f13:0x00400000" ];
   hardware.i2c.enable = true;
   
   services.hardware.openrgb = { 
