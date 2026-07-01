694a4c41ed9492f243d4dbb485c0b84cfef2a8aa
Daniel <accrrsd@bk.ru>
2026-07-01 10:10:31 +0300

    remove hyprland delay

--- user-shared/accrrsd/app/hyprland/config/lua/submaps.lua
+++ user-shared/accrrsd/app/hyprland/config/lua/submaps.lua
@@ -1,15 +1,7 @@
 local gaming_key = "F19"
 local is_gaming_mode = false
-local last_toggle_time = 0
-
-hl.bind("CTRL + mouse:272", hl.dsp.window.drag(), { mouse = true })
-hl.bind("CTRL + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 local function toggle_gaming_mode()
-    local current_time = os.clock()
-    if (current_time - last_toggle_time) < 0.2 then return end
-    last_toggle_time = current_time
-
     if not is_gaming_mode then
         is_gaming_mode = true
         hl.unbind("CTRL + mouse:272")
@@ -23,4 +15,6 @@ local function toggle_gaming_mode()
     end
 end
 
+hl.bind("CTRL + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind("CTRL + mouse:273", hl.dsp.window.resize(), { mouse = true })
 hl.bind(gaming_key, toggle_gaming_mode)
